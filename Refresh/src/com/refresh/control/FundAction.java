665ddb7616c96ccf96199ad8a568bc0807564f28
kosta <kosta@192.168.0.117>
2017-06-09 14:10:56 +0900

    fund action 수정

--- Refresh/src/com/refresh/control/FundAction.java
+++ Refresh/src/com/refresh/control/FundAction.java
@@ -1,8 +1,11 @@
 package com.refresh.control;
 
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
@@ -82,6 +85,88 @@ public class FundAction extends Action{
 			case "update" : //수정
 				break;
 			case "delete" : //삭제
+				break;
+			case "main" :
+				String pageStr = request.getParameter("page");
+				
+				int page;
+				if(pageStr==null){
+					page = 1;
+				}else{
+					page = Integer.parseInt(pageStr);
+				}
+						/*
+						 한페이지(한 화면)에 보여질 레코드 수 : currRecord
+						 전체 레코드 수 : totalRecord
+						 한 화면에 보여질 페이지 수 : currPage
+						 전체 페이지 수 : totalPage
+						 */
+			
+				String sort = request.getParameter("sort");
+				if(sort==null){
+					sort = "all";
+					}
+				int recordCount = 3; //한 페이지에 보일 게시물 갯수
+				int currPage = 5; //보일 페이지 갯수
+						
+				int totalRecord = 0; //총 게시물 갯수
+				if(sort.equals("all")){
+					totalRecord = dao.selectTotal();
+				}else if(sort.equals("ended")){
+					totalRecord = dao.selectEndedTotal();
+				}else if(sort.equals("close")){
+					totalRecord = dao.selectCloseTotal();
+				}else{
+					//특정 카테고리
+					totalRecord = dao.selectCategoryTotal(sort);
+				}
+						
+				int totalPage =  1;
+				int startPage = 1;
+				int endPage = 1;
+				//게시물이 0개일 경우 기본값
+				if(totalRecord!=0){	
+					totalPage = 0; //총 페이지 갯수
+					startPage = ((page-1)/currPage*currPage)+1; 
+					// 시작블럭숫자 (1~5페이지일경우 1, 6~10일경우 6) 
+					endPage = ((page-1)/currPage*currPage)+currPage; 
+					// 끝 블럭 숫자 (1~5일 경우 5, 6~10일경우 10
+					totalPage = (int)Math.ceil(totalRecord/(double)recordCount);
+					if(endPage > totalPage) {
+						endPage = totalPage;
+						}
+					}
+
+					//DB에서 검색
+					List<Funding> list = null;
+					if(sort.equals("all")){
+						list = dao.selectPageAll(page,recordCount);
+					}else if(sort.equals("ended")){
+						list = dao.selectEnded(page, recordCount);
+					}else if(sort.equals("close")){
+						list = dao.selectClose(page, recordCount);
+					}else{
+						//특정 카테고리
+						list = dao.selectCategory(page, recordCount, sort);
+					}
+						
+					if(list!=null){
+						for(int i=0; i<list.size(); i++){
+							request.setAttribute("sort", sort);
+							request.setAttribute("list", list);
+							request.setAttribute("page", page);
+							request.setAttribute("startPage", startPage);
+							request.setAttribute("endPage", endPage);
+							request.setAttribute("totalPage", totalPage);
+							}//for
+						}//if
+					forward = mapping.findForward("success");
+				   break;
+			case "logout" : 
+				HttpSession session = request.getSession();
+				session.invalidate();
+				
+				forward = mapping.findForward("logout");
 				break;
 		
 		}
